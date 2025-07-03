047a684353fc5cd020fe49226c2d7c8ea19fbf4a
oshiyani E <oshiyani.e2023cse@sece.ac.in>
2025-07-03 09:04:44 +0530

    second commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+    public Student() {
+        System.out.println("Inside Student Class");
+    }
+    public void internshipProgram(){
+        System.out.println("Students are attending SpringBoot  inside CodeStudio");
+    }
+}
